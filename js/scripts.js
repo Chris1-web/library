3ead9940aaf528b6c09750f0f6ad2d836f49e727
Christianah Adeyemo <christianah123.ac@gmail.com>
2022-03-18 00:48:10 +0100

    completed ui/ux proposed design

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,30 @@
 const newBookBtn = document.querySelector(".add-btn");
+const overlay = document.querySelector(".open-form");
+const newBookForm = document.querySelector(".add-a-new-book-form");
+const closeBtn = document.querySelector(".closeBtn");
 
-newBookBtn.addEventListener("click", function () {
-  alert("new book to be added");
-});
+//show add a new book form after clicking add a new book button
+// const showAddNewBookForm = function () {
+//   newBookBtn.addEventListener("click", function () {
+//     overlay.classList.toggle("open");
+//     newBookForm.classList.toggle("open");
+//   });
+// };
+
+const closeFormAndOverlay = function () {
+  overlay.classList.remove("open");
+  newBookForm.classList.remove("open");
+};
+
+const showAddNewBookForm = function () {
+  overlay.classList.toggle("open");
+  newBookForm.classList.toggle("open");
+};
+
+overlay.addEventListener("click", showAddNewBookForm);
+closeBtn.addEventListener("click", showAddNewBookForm);
+newBookBtn.addEventListener("click", showAddNewBookForm);
+
+// showAddNewBookForm();
+// // closeAddNewBookForm();
+// closeAddNewBookFormBtn();
