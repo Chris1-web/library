2598d1f52ab7d328b60cd9b23076dca26de6827a
Christianah Adeyemo <christianah123.ac@gmail.com>
2022-03-18 15:04:22 +0100

    completed remove card button

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,7 @@ const bookTitle = document.querySelector("#title");
 const bookAuthor = document.querySelector("#author");
 const bookNumber = document.querySelector("#number-of-pages");
 const bookStatus = document.querySelector("#status");
+const allCards = document.querySelector(".cards");
 
 const showAddNewBookForm = function () {
   overlay.classList.toggle("open");
@@ -36,6 +37,7 @@ form.addEventListener("submit", function (e) {
   addBookToLibrary(book);
   createCard(book);
   clearForm();
+  showAddNewBookForm();
 });
 
 const clearForm = function () {
@@ -56,15 +58,51 @@ const createCard = function (book) {
   const cardHeader = document.createElement("h1");
   cardHeader.style.textAlign = "center";
   cardHeader.textContent = book.title;
+  cardHeader.classList.add("card-title");
   const paragraph = document.createElement("p");
   paragraph.textContent = book.author;
+  paragraph.classList.add("card-author");
   const buttonDivContainer = document.createElement("div");
   buttonDivContainer.classList.add("card-button");
   const ReadBtn = document.createElement("button");
+  ReadBtn.classList.add("readBtn");
   ReadBtn.textContent = `${book.hasRead === "Yes" ? "Read" : "Not Yet"}`;
   const RemoveBtn = document.createElement("button");
   RemoveBtn.textContent = "Remove";
+  RemoveBtn.classList.add("removeBtn");
   buttonDivContainer.append(ReadBtn, RemoveBtn);
   divContainer.append(cardHeader, paragraph, buttonDivContainer);
-  console.log(cardsContainer);
 };
+
+const toggleRead = function (e) {
+  if (e.target.textContent === "Read") e.target.textContent = "Not Yet";
+  else e.target.textContent = "Read";
+};
+
+const removeBook = function (e) {
+  // get current clicked card name and author and filter it out of the remaining library content
+  const cardTitleText = e.target.closest(".card").firstElementChild.textContent;
+  const cardAuthorText =
+    e.target.closest(".card").firstElementChild.nextElementSibling.textContent;
+  e.target.textContent = "Removed";
+  //  get the cards index
+  const selectedCardIndex = myLibrary.findIndex((book) => {
+    return book.title === cardTitleText && book.author === cardAuthorText;
+  });
+  // remove the card permernently from MyLibrary array
+  myLibrary.splice(selectedCardIndex);
+  e.target.closest(".card").remove();
+};
+
+// if card element is clicked
+allCards.addEventListener("click", function (e) {
+  // if it is book status button,toggle text content
+  if (e.target.classList.contains("readBtn")) {
+    toggleRead(e);
+    return;
+  }
+  //else if it is remove button, remove card element
+  else if (e.target.classList.contains("removeBtn")) {
+    removeBook(e);
+  } else return;
+});
