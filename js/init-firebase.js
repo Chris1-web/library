ddec438d898f41a130201d77c29051bb81924c43
Christianah Adeyemo <christianah123.ac@gmail.com>
2022-07-07 23:45:41 +0100

    create firebase initialization

---
+++ js/init-firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBZOxgM-DfyNhnXWX9xYpnDm5-wmZdGdI8",
+  authDomain: "library-1a246.firebaseapp.com",
+  projectId: "library-1a246",
+  storageBucket: "library-1a246.appspot.com",
+  messagingSenderId: "991717499238",
+  appId: "1:991717499238:web:7b9a57bdc4f62cefc068a7",
+};
+firebase.initializeApp(firebaseConfig);
--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,35 @@ const bookAuthor = document.querySelector("#author");
 const bookNumber = document.querySelector("#number-of-pages");
 const bookStatus = document.querySelector("#status");
 const allCards = document.querySelector(".cards");
+const loginButton = document.querySelector(".login");
 let book;
+
+loginButton.addEventListener("click", () => {
+  const auth = firebase.auth();
+  const provider = new firebase.auth.GoogleAuthProvider();
+  auth
+    .signInWithPopup(provider)
+    .then((result) => {
+      /** @type {firebase.auth.OAuthCredential} */
+      var credential = result.credential;
+
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      // ...
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
+});
 
 const showAddNewBookForm = () => {
   overlay.classList.toggle("open");
