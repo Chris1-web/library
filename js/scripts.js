d4b4d39f4aae008439d2f1ec353fa5f1d5c40880
Christianah Adeyemo <christianah123.ac@gmail.com>
2022-07-08 14:09:30 +0100

    seperate logic

--- js/scripts.js
+++ js/scripts.js
@@ -12,120 +12,8 @@ const loginButton = document.querySelector(".login");
 const logoutButton = document.querySelector(".logout");
 const profileImage = document.querySelector(".profile-img");
 const userName = document.querySelector(".user-name");
+const loader = document.querySelector(".loader-container");
 let book;
-
-function initFirebaseAuth() {
-  // Listen to auth state changes. called at the buttom at all times
-  auth.onAuthStateChanged((user) => {
-    if (user) {
-      // if user is available, hide login button and get user details to display
-      userName.textContent = getUserName();
-      profileImage.src = getProfileUrl();
-
-      userName.classList.remove("hide");
-      profileImage.classList.remove("hide");
-      logoutButton.classList.remove("hide");
-      loginButton.classList.add("hide");
-      loadBooks();
-    } else {
-      loginButton.classList.remove("hide");
-      logoutButton.classList.add("hide");
-      userName.classList.add("hide");
-      profileImage.classList.add("hide");
-    }
-  });
-}
-initFirebaseAuth();
-
-const getProfileUrl = function () {
-  return auth.currentUser.photoURL;
-};
-
-const getUserName = function () {
-  return auth.currentUser.displayName;
-};
-
-const isUserSignedIn = function () {
-  return !!auth.currentUser;
-};
-
-const checkSignedIn = function (e) {
-  e.preventDefault();
-  if (isUserSignedIn()) {
-    return true;
-  }
-  return false;
-};
-
-const googleSignIn = async function () {
-  const provider = new firebase.auth.GoogleAuthProvider();
-  await auth.signInWithPopup(provider);
-  const cardsContainer = document.querySelector(".cards");
-  cardsContainer.textContent = "";
-};
-
-const googleSignOut = function () {
-  auth.signOut();
-  const cardsContainer = document.querySelector(".cards");
-  cardsContainer.textContent = "";
-};
-
-const saveBookToDB = function (e) {
-  let title = bookTitle.value;
-  let author = bookAuthor.value;
-  let pages = bookNumber.value;
-  let status = bookStatus.value;
-  book = createBookObject(title, author, pages, status);
-  db.collection("books").add({
-    owner: auth.currentUser.uid,
-    title: book.title,
-    author: book.author,
-    pagesNum: book.pagesNum,
-    hasRead: book.hasRead,
-  });
-  clearForm();
-  showAddNewBookForm();
-};
-
-const displayCardFromDB = function (book) {
-  const cardsContainer = document.querySelector(".cards");
-  const html = ` 
-      <div class="card">
-        <h1 class="card-title">${book.title}</h1>
-        <p class="card-author">${book.author}</p>
-        <div class="card-button">
-          <button class="readBtn">${
-            book.hasRead === "Yes" ? "Read" : "Not Read"
-          }</button>
-          <button class="removeBtn">Remove</button>
-        </div>
-      </div>
-    `;
-  cardsContainer.insertAdjacentHTML("beforeend", html);
-};
-
-const loadBooks = function () {
-  db.collection("books")
-    .where("owner", "==", auth.currentUser.uid)
-    .onSnapshot((snapshot) => {
-      snapshot.docChanges().forEach((change) => {
-        if (change.type === "added") {
-          const book = change.doc.data();
-          console.log(book);
-          displayCardFromDB(book);
-        }
-        if (change.type === "removed") {
-          const book = change.doc.data();
-          // search all h1 elements and remove the clicked one
-          document.querySelectorAll("h1").forEach(function (element) {
-            if (element.textContent === book.title) {
-              element.closest(".card").remove();
-            }
-          });
-        }
-      });
-    });
-};
 
 loginButton.addEventListener("click", googleSignIn);
 logoutButton.addEventListener("click", googleSignOut);
@@ -222,23 +110,12 @@ const getCurrentCardAuthor = function (e) {
   return cardAuthorText;
 };
 
-const removeBookFromDB = function (e, bookTitle) {
-  const selectedBook = db
-    .collection("books")
-    .where("owner", "==", auth.currentUser.uid)
-    .where("title", "==", bookTitle);
-  selectedBook.get().then(function (querySnapshot) {
-    querySnapshot.forEach(function (doc) {
-      doc.ref.delete();
-    });
-  });
-};
-
 const removeBook = (e) => {
   // get current clicked card name and author and find its index in the myLibrary array
   const curTitle = getCurrentCardTitle(e);
   const curAuthor = getCurrentCardAuthor(e);
 
+  // if user is signed in, remove from the database
   if (checkSignedIn(e)) {
     removeBookFromDB(e, curTitle);
   } else {
